bd7ae7d8b7d9cc50873b45a2b4eff5a02f386c2a
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-12-13 16:17:57 +0900

    6. Creating the Card Components

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroSection from "../HeroSection";
+import Cards from "../Cards";
 import "../../App.css";
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 }
